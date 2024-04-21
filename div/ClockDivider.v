6dabf67da404f0ad667c46dd25a011e1fb9e1756
Marco Chen <chenxuanlin2001@bwrcrdsl-1.eecs.berkeley.edu>
2024-04-20 17:23:06 -0700

    divider spice model fixed

--- div/ClockDivider.v
+++ div/ClockDivider.v
@@ -16,59 +16,51 @@ module DivLatch2(
     end
   end
 endmodule
-module ClockMux(
-  input   io_clocksIn_1,
-  output  io_clockOut
+
+module Mux2(
+  input input1,
+  input input2,
+  output result,
+  input choice
 );
-  assign io_clockOut = io_clocksIn_1; // @[div.scala 35:17]
+  reg out = 0;
+  assign result = out;
+  always@(*) begin
+    if(choice==0) begin
+      out = input1;
+    end else begin
+      out = input2;
+    end
+  end
 endmodule
-module ClockMuxTree(
-  input   io_in_clks_1,
-  input   io_in_clks_4,
-  output  io_out_clk
+
+module MuxTree(
+  input div0,
+  input div2,
+  input div4,
+  input div8,
+  input div16,
+  input div32,
+  input [2:0] sel,
+  output result
 );
-  wire  io_out_clk_mux_io_clocksIn_1; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clockOut; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_0_mux_io_clocksIn_1; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_0_mux_io_clockOut; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clockOut; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux_io_clocksIn_1; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux_io_clockOut; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clocksIn_1; // @[div.scala 84:21]
-  wire  io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clockOut; // @[div.scala 84:21]
-  ClockMux io_out_clk_mux ( // @[div.scala 84:21]
-    .io_clocksIn_1(io_out_clk_mux_io_clocksIn_1),
-    .io_clockOut(io_out_clk_mux_io_clockOut)
-  );
-  ClockMux io_out_clk_mux_io_clocksIn_0_mux ( // @[div.scala 84:21]
-    .io_clocksIn_1(io_out_clk_mux_io_clocksIn_0_mux_io_clocksIn_1),
-    .io_clockOut(io_out_clk_mux_io_clocksIn_0_mux_io_clockOut)
-  );
-  ClockMux io_out_clk_mux_io_clocksIn_1_mux ( // @[div.scala 84:21]
-    .io_clocksIn_1(io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1),
-    .io_clockOut(io_out_clk_mux_io_clocksIn_1_mux_io_clockOut)
-  );
-  ClockMux io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux ( // @[div.scala 84:21]
-    .io_clocksIn_1(io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux_io_clocksIn_1),
-    .io_clockOut(io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux_io_clockOut)
-  );
-  ClockMux io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux ( // @[div.scala 84:21]
-    .io_clocksIn_1(io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clocksIn_1),
-    .io_clockOut(io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clockOut)
-  );
-  assign io_out_clk = io_out_clk_mux_io_clockOut; // @[div.scala 101:14]
-  assign io_out_clk_mux_io_clocksIn_1 = io_out_clk_mux_io_clocksIn_1_mux_io_clockOut; // @[div.scala 96:26]
-  assign io_out_clk_mux_io_clocksIn_0_mux_io_clocksIn_1 = io_in_clks_1; // @[div.scala 87:73]
-  assign io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1 = io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clockOut
-    ; // @[div.scala 96:26]
-  assign io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_0_mux_io_clocksIn_1 = 1'h0; // @[div.scala 89:48]
-  assign io_out_clk_mux_io_clocksIn_1_mux_io_clocksIn_1_mux_io_clocksIn_1 = io_in_clks_4; // @[div.scala 87:73]
+  wire out01;
+  wire out02;
+  wire out03;
+  wire out11;
+
+  Mux2 mux01(.input1(div0), .input2(div2), .result(out01), .choice(sel[0]));
+  Mux2 mux02(.input1(div4), .input2(div8), .result(out02), .choice(sel[0]));
+  Mux2 mux03(.input1(div16), .input2(div32), .result(out03), .choice(sel[0]));
+  Mux2 mux11(.input1(out01), .input2(out02), .result(out11), .choice(sel[1]));
+  Mux2 mux21(.input1(out11), .input2(out03), .result(result), .choice(sel[2]));
+
 endmodule
+
 module ClockDivider(
   input        clock,
   output       io_div_clk_div,
-  input  [4:0] io_sel_clk_div,
+  input  [2:0] io_sel_clk_div,
   input        io_rst,
   input        io_en
 );
@@ -92,8 +84,6 @@ module ClockDivider(
   wire  clk_div_map_4_io_input_clock; // @[div.scala 117:62]
   wire  clk_div_map_4_io_div2_clk; // @[div.scala 117:62]
   wire  clk_div_map_4_io_en; // @[div.scala 117:62]
-  wire  div_mux_tree_io_in_clks_1; // @[div.scala 134:30]
-  wire  div_mux_tree_io_in_clks_4; // @[div.scala 134:30]
   wire  div_mux_tree_io_out_clk; // @[div.scala 134:30]
   DivLatch2 clk_div_map_0 ( // @[div.scala 117:62]
     .io_rst(clk_div_map_0_io_rst),
@@ -125,11 +115,16 @@ module ClockDivider(
     .io_div2_clk(clk_div_map_4_io_div2_clk),
     .io_en(clk_div_map_4_io_en)
   );
-  ClockMuxTree div_mux_tree ( // @[div.scala 134:30]
-    .io_in_clks_1(div_mux_tree_io_in_clks_1),
-    .io_in_clks_4(div_mux_tree_io_in_clks_4),
-    .io_out_clk(div_mux_tree_io_out_clk)
-  );
+  MuxTree mux(
+  .div0(clock),
+  .div2(clk_div_map_0_io_div2_clk),
+  .div4(clk_div_map_1_io_div2_clk),
+  .div8(clk_div_map_2_io_div2_clk),
+  .div16(clk_div_map_3_io_div2_clk),
+  .div32(clk_div_map_4_io_div2_clk),
+  .sel(io_sel_clk_div),
+  .result(div_mux_tree_io_out_clk)
+);
   assign io_div_clk_div = div_mux_tree_io_out_clk; // @[div.scala 137:20]
   assign clk_div_map_0_io_rst = io_rst; // @[div.scala 124:34]
   assign clk_div_map_0_io_input_clock = clock; // @[div.scala 128:26]
@@ -146,6 +141,4 @@ module ClockDivider(
   assign clk_div_map_4_io_rst = io_rst; // @[div.scala 124:34]
   assign clk_div_map_4_io_input_clock = clk_div_map_3_io_div2_clk; // @[div.scala 128:26]
   assign clk_div_map_4_io_en = io_en; // @[div.scala 123:33]
-  assign div_mux_tree_io_in_clks_1 = clk_div_map_1_io_div2_clk; // @[div.scala 115:28 120:47]
-  assign div_mux_tree_io_in_clks_4 = clk_div_map_4_io_div2_clk; // @[div.scala 115:28 126:23]
 endmodule
